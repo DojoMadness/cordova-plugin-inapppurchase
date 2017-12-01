02a70337b4850e7a41659ca4b1912218591b8b3a
Patrick Thonhauser <patrick.thonhauser@gmail.com>
2017-12-01 09:33:06 +0100

    add function prototype

--- src/ios/PaymentsPlugin.h
+++ src/ios/PaymentsPlugin.h
@@ -16,5 +16,6 @@
 - (void)buy:(CDVInvokedUrlCommand *)command;
 - (void)restorePurchases:(CDVInvokedUrlCommand *)command;
 - (void)getReceipt:(CDVInvokedUrlCommand *)command;
+- (void)getReceiptWithoutForceRefresh:(CDVInvokedUrlCommand *)command
 
 @end
